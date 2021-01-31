d1a99e9870ebccf0eb74cadf1ab6f2e5023fcc1d
Sohibjon <sohibjonsa@gmail.com>
2021-01-31 17:20:37 +0500

    UpdateFirstname

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ token = '1583466472:AAFxCsiR1XSl_oUaIRIfdfa2DOINEBzdnVE'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
 data = r.json()
+updates = data['result']
 
-pprint(data)
+for update in updates:
+    message = update['message']
+    user = message['from']
+    name = user['first_name']
+    print(name)
