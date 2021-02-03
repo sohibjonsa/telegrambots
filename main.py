575bf614687ae431cefe1863ed8ab5011e955da7
Sohibjon <sohibjonsa@gmail.com>
2021-02-03 15:42:50 +0500

    UpdateWithGet

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import requests
 from pprint import pprint
 
-token = '1583466472:AAFxCsiR1XSl_oUaIRIfdfa2DOINEBzdnVE'
+token = '1583466472:AAGguScK6YXMLPCiRKN01kbEkviCw92wmqw'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
 data = r.json()
-updates = data['result']
+# updates = data['result']
 
-for update in updates:
-    message = update['message']
-    user = message['from']
-    first = user.get('first_name', '')
-    last = user.get('last_name', '')
-    print(f'{first} {last}')
+# for update in updates:
+#     message = update['message']
+#     user = message['from']
+#     first = user.get('first_name', '')
+#     last = user.get('last_name', '')
+#     print(f'{first} {last}')
+
+pprint(data)
+print(r.url)
