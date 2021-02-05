59e83ca389db444394de8b6d568f4cd9772c1d94
Sohibjon <sohibjonsa@gmail.com>
2021-02-06 00:09:08 +0500

    sendMessages

--- sendMessage.py
+++ sendMessage.py
@@ -3,4 +3,13 @@ from pprint import pprint
 
 token = '1583466472:AAGguScK6YXMLPCiRKN01kbEkviCw92wmqw'
 url = f'https://api.telegram.org/bot{token}/sendMessage'
-r = requests.get(url)
+p = {
+    'chat_id': 81633833,
+    'text': 'Yaxshimisiz?'
+}
+r = requests.get(url, params=p)
+
+print(r.json())
+
+print(r.url)
+
