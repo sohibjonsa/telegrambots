53038f1efbdd181cd998c664df32dddc7c6328da
sohibjonsa <sohibjonsa@gmail.com>
2021-02-07 15:33:02 +0500

    updating

---
+++ send_mdwn.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import os
+from pprint import pprint
+
+
+token = os.environ['TOKEN']
+
+def sendMsg(idx, txt):
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    p = {
+        'chat_id': idx,
+        'text': txt
+        'parse_mode':'MarkdownV2'
+    }
+    r = requests.get(url, params=p)
+   
+
+sendMsg(81633833, '**Salom**')
