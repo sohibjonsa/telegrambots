4b053de4fccd8d4129a2df205c0f59a916aaf0dc
Sohibjon <sohibjonsa@gmail.com>
2021-02-07 17:23:18 +0500

    Dice

---
+++ sendDice.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from pprint import pprint
+import os
+
+token =os.environ['TOKEN'] 
+
+def sendDice(idx):
+    url = f'https://api.telegram.org/bot{token}/sendDice'
+    payload = {
+    'chat_id':idx,
+    }
+    r = requests.get(url,params=payload)
+    return r.json()
+
+
+ids= 81633833
+sendDice(ids)
