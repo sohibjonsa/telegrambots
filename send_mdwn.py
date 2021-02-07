db877b96a88072773559a32f511dce974796b5d4
sohibjonsa <sohibjonsa@gmail.com>
2021-02-07 15:48:35 +0500

    Markdown sending

--- send_mdwn.py
+++ send_mdwn.py
@@ -10,10 +10,10 @@ def sendMsg(idx, txt):
     url = f'https://api.telegram.org/bot{token}/sendMessage'
     p = {
         'chat_id': idx,
-        'text': txt
-        'parse_mode':'MarkdownV2'
+        'text': txt,
+        'parse_mode': 'MarkdownV2'
     }
     r = requests.get(url, params=p)
    
 
-sendMsg(81633833, '**Salom**')
+sendMsg(81633833, '*Salom*')
